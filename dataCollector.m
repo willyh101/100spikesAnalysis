7741711a276617e0ad613f8a45c96acf31baa901
iawn <ian.oldenburg@gmail.com>
2020-01-14 14:46:17 -0800

    Handling of incomplete datasets

--- dataCollector.m
+++ dataCollector.m
@@ -1,4 +1,11 @@
 %% general info
+
+if ~exist('ExpStruct')
+    disp('Need to reload ExpStruct...')
+in = load(physfile,'ExpStruct');
+ExpStruct=in.ExpStruct;
+end
+
 if length(date)>6
     info.date = date(3:end);
 else
@@ -10,15 +17,19 @@ info.epochText2 = ExpStruct.EpochText2;
 info.params = ExpStruct.Expt_Params;
 info.path = path;
 info.outputNames = ExpStruct.output_names;
+info.FR=FR;
+
 disp('got info')
 
-%% experiment epoch
+%%experiment epoch
 exp.zdfData = zdfData;
 exp.allData = allData;
 exp.runVal = runVector;
 exp.lowMotionTrials = lowMotionTrials;
 exp.stimID = stimID;
+try
 exp.visID = visID;
+catch;end
 exp.holoTargets = HoloTargets;
 exp.rois = roisTargets;
 exp.allCoM = allCoM;
@@ -26,10 +37,16 @@ exp.allDepth = allDepth;
 exp.stimCoM = stimCoM;
 exp.stimDepth = stimDepth;
 exp.targetedCells = targettedCells;
+exp.outputsInfo = outputPatternTranslator(ExpStruct,uniqueStims);
+
 exp.stimParams = stimParam;
+try
 exp.stimParams.Hz = holoRequests.holoStimParams.hzList;
 exp.stimParams.numCells = holoRequests.holoStimParams.cellsPerHolo;
 exp.stimParams.powers = holoRequests.holoStimParams.powerList;
+catch
+    disp('no holoStimParams')
+end
 disp('got exp')
 
 %% orientation/vis epoch
@@ -38,13 +55,19 @@ vis.allData = allData;
 vis.runVal = runVector;
 vis.lowMotionTrials = lowMotionTrials;
 vis.visID = visID;
+vis.visStart = visStart;
+vis.visStop = visStop; 
 disp('got vis')
 
 %% run to save
 
 out.info = info;
 out.exp = exp;
-out.vis = vis;
+try
+    out.vis = vis;
+catch
+    disp('No Vis Data')
+end
 
 save([basePath info.date '_' info.mouse '_outfile'], 'out')
 save(['Z:\willh\outputdata\' info.date '_' info.mouse 'outfile'], 'out')
