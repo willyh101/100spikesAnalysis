ede081133c5eb3b1f04f4ec60619bbf4ac72a0f7
iawn <ian.oldenburg@gmail.com>
2020-01-21 18:01:05 -0800

    initial commit
    update file

---
+++ calcL1L2.m
@@ -0,0 +1,11 @@
+function [L1 L2 L3] =  calcL1L2(testData,ExpectedData)
+%return the L1 and L2 values for a dataset size Cells x Obs
+
+meanResp = mean(ExpectedData,2);
+
+Err = meanResp - testData ;
+Err = mean(Err'); 
+
+L1 = sum(abs(Err));
+L2 = sqrt(sum(Err.^2));
+L3 = sum(abs(Err).^3).^(1/3);
