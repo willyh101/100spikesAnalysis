6b464d58f7b2797c78f7a20e7c9285a90dc2f3d3
iawn <ian.oldenburg@gmail.com>
2020-01-30 19:24:13 -0800

    added toggle between evoked and not evoked sparsity

--- calcL1L2.m
+++ calcL1L2.m
@@ -1,10 +1,15 @@
-function [L1 L2 L3] =  calcL1L2(testData,ExpectedData)
+function [L1 L2 L3] =  calcL1L2(testData,ExpectedData,method)
 %return the L1 and L2 values for a dataset size Cells x Obs
-
+if nargin <3
+    method =1;
+end
 meanResp = mean(ExpectedData,2);
 
+if method==1
 Err = meanResp - testData ;
-% Err = testData; %dan says this should be included, so you look at the
+else
+Err = testData; %dan says this should be included, so you look at the
+end
 % population sparsity instead of the sparsity of the change
 Err = mean(Err'); 
 
