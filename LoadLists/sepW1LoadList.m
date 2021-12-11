7061b1655780e7e00507a0528d74c677894081ef
iawn <ian.oldenburg@gmail.com>
2021-12-10 17:35:30 -0800

    opsin negative sepW1 analysis

--- LoadLists/sepW1LoadList.m
+++ LoadLists/sepW1LoadList.m
@@ -2,7 +2,11 @@
 loadListName = 'sepW1LoadList'; 
 
 loadList = {
-   '211014_I156_1_outfile.mat'
-   '211019_I156_1_outfile.mat'
-   '211102_I158_1_outfile.mat'
+    '211108_I156_1_outfile.mat' %sepW1
+    '211014_I156_1_outfile.mat'
+    '211019_I156_1_outfile.mat'
+    
+    '211102_I158_1_outfile.mat'
+    
+    '211206_I162_1_outfile.mat' %very small dataset
 };
